e953b73c2c97ff318bb6474fe3922399d85c3d8e
Unknown <wcarrasco@mail.csuchico.edu>
2017-03-07 23:57:44 -0800

    commit

--- form-validation.js
+++ form-validation.js
@@ -1,7 +1,13 @@
 function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
+  var str = "myusername";
+  var n = str.includes(x>6);
+  document.getElementById("usernameGroup").innerHTML="n";
   var passEntered = document.getElementById("pass").value;
+  var str = "mypassword";
+  var n = str.includes("password")
+  document.getElementById("passwordGroup").innerHTML="n";
 
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
@@ -9,6 +15,7 @@ function validate(){
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
+  document.getElementById("usernameGroup").classList.add("has-success");
 
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
@@ -16,4 +23,5 @@ function validate(){
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
 }
